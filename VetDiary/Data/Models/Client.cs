c142f6e0f98909442cf7011d811adea3c8f2e608
Anita Kirilova <anita_kirilova@yahoo.com>
2026-02-04 23:53:22 +0200

    Create client, add migration, controller and view with scaffolding

---
+++ VetDiary/Data/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VetDiary.Data.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+
+    }
+}
