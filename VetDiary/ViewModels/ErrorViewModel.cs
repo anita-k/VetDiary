e049a573638c8bffc5bc9485bd88a4d7e6cbf899
Anita Kirilova <anita_kirilova@yahoo.com>
2026-02-04 23:52:04 +0200

    Add solution and project files with MS Identity

---
+++ VetDiary/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VetDiary.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
